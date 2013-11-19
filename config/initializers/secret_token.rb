9e79469b54e57dfd656673d664f2e839337d9556
unknown <lrobinson@EXO-LROBINSON.jta.coleman.com>
2013-11-19 15:17:36 -0800

    first material db commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExoMaterial::Application.config.secret_token = '362c924989ace150dba62bd19282b23554c4fcead933b391cc43b97e334efee4e13e5edd3ca643a1ec57ad7ddc40e56788159f6fd96ab45b4e7aa115e99f2427'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ExoMaterial::Application.config.session_store :cookie_store, key: '_exo-material_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ExoMaterial::Application.config.session_store :active_record_store
